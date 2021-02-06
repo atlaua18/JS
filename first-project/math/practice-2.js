692a786c5c2879abfc0849bc4f00285c1f3f8988
Anastasia <my email address used on my git repo>
2021-02-07 02:12:35 +0300

    add math

---
+++ first-project/math/practice-2.js
@@ -0,0 +1,18 @@
+/* 
+Вычисляем  расстояние между двумя точками
+Даны координаты x, y 2-x точек. Нужно вывести расстояние между ними.
+Высясляем по теореме Пифагора 
+*/
+
+let x1 = 10;
+let y1 = 2;
+
+let x2 = -3;
+let y2 = 3;
+
+let cathetus1 = Math.abs(x1 - x2);
+let cathetus2 = Math.abs(y1 - y2);
+
+console.log(Math.sqrt(
+    Math.pow(cathetus1, 2) + Math.pow(cathetus2, 2)
+));
