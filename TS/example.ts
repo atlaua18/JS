8d398c2263a872cf90d7ae1a24cad8b6754b3261
atlaua18 <nastyaopen@yandex.ru>
2021-04-27 17:43:34 +0300

    todo http response

---
+++ TS/example.ts
@@ -0,0 +1,15 @@
+// JS Types
+//typeof
+
+const str = 'string'; // typeof srt -> 'string'
+const num = 2; // typeof num -> 'number'
+const nan = NaN; // typeof nan -> 'number'
+const obj = {}; // typeof obj -> 'object'
+const arr = []; // typeof arr -> 'object'
+const nul = null; // typeof null -> 'object'
+const sym = Symbol(); // typeof sym -> 'symbol'
+const und = undefined; // typeof und -> 'undefined'
+const _void = void 0; // typeof _void -> 'undefined'
+const bool = true; // typeof bool -> 'boolean'
+const fn = () => {}; // typeof fn -> 'function'
+
